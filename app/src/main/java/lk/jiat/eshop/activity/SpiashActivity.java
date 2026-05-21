73ab078edcb3b7256c259b374e5d3327ff0528bd
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-21 14:09:30 +0530

    create SpiashActivity

---
+++ app/src/main/java/lk/jiat/eshop/activity/SpiashActivity.java
@@ -0,0 +1,26 @@
+package lk.jiat.eshop.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import lk.jiat.eshop.R;
+
+public class SpiashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_spiash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
